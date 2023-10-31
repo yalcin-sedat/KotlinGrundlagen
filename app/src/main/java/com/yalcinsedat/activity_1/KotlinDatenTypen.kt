7d62ef6ad64e0f67555177a6f0c860ee1fbb18c7
sedat yalcin <andrroid.developer@gmail.com>
2023-10-31 15:01:26 +0100

    initial Commit Kotlin Grundlagen 2-Datentypen (Boolen,Char, String,Byte,Short,Int,Long,Float, Double)

---
+++ app/src/main/java/com/yalcinsedat/activity_1/KotlinDatenTypen.kt
@@ -0,0 +1,39 @@
+package com.yalcinsedat.activity_1
+
+fun main(){
+
+    /*
+    val myName="Sedat"
+    var surName="Kaya"
+     */
+
+    var myAge=21
+    println(myAge)
+
+    //Integer TYPES: Byte(8 bit), Short (16 bit), Int (32 bit), Long (64 bit)
+    val myByte:Byte=13
+    val myShort: Short = 125
+    val myInt : Int = 123456789
+    val myLong : Long = 1234567890123456789
+
+    //Floating Point number Types: Float (32 bit), Double (64 bit)
+    val myFloat:Float =14.34F
+    val myDouble: Double= 3.234567876543
+
+    var myBool= true
+    myBool=false
+
+    var letterChar= 'S'
+    var digitChar = '2'
+    val myString= "Hallo Android"
+    var firstChar =myString[0]
+    var lastChar =myString[myString.length-1]
+
+    println(firstChar)
+    println(lastChar)
+
+
+
+
+
+}
