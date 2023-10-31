5dcc0366ebe15be9e15e75ee49f3b429bb4d7eec
sedat yalcin <andrroid.developer@gmail.com>
2023-10-31 17:33:25 +0100

    initial Commit Kotlin Grundlagen 3-Arithmetische Operatoren + ,- , * ,/, %

---
+++ app/src/main/java/com/yalcinsedat/activity_1/ArithmetischeOperation.kt
@@ -0,0 +1,27 @@
+package com.yalcinsedat.activity_1
+
+fun main(){
+    //Arithmetische Operatoren (+ ,- , * ,/, % )
+
+    var num=10
+    var num2=20
+    var result = num+num2
+
+    println("Result : $result")
+    //--------------------------
+    val num3=10
+    val num4=5
+    val result_1=(( num3 / num4)*num)%3
+    println("Result 1 : "+result_1)
+    println("Result + Result_1 = $result+$result_1")
+    //-----------------------------------
+    var num5: Double=30.3
+    var num6: Double=40.0
+
+    println("Summe = $num5 + $num6")
+
+
+
+
+
+}
