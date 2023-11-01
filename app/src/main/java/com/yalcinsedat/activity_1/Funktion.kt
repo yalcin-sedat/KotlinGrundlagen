1115af9019e1d34d20daa21753940a67c68cbef4
sedat yalcin <andrroid.developer@gmail.com>
2023-11-01 17:22:11 +0100

    initial Commit 7- Kotlin Grundlagen  -> Funktion !

---
+++ app/src/main/java/com/yalcinsedat/activity_1/Funktion.kt
@@ -0,0 +1,14 @@
+package com.yalcinsedat.activity_1
+
+fun main() {
+    println("Funktion Main")
+
+    var ergebnis= multiplizieren(addieren(3,6),6)
+    println(ergebnis.toString())
+}
+    fun addieren(a:Int ,b:Int):Int{
+        return a+b
+    }
+    fun multiplizieren(a:Int ,b:Int):Int{
+        return a*b
+    }
