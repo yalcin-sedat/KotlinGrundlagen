172d9ee317962c073e97cf1e7c53e4ef6ebbb2f5
sedat yalcin <andrroid.developer@gmail.com>
2023-11-09 09:14:20 +0100

    10- Kotlin Grundlagen  -> Mitgliedsvariablen - Funktionen und Konstruktor !

---
+++ app/src/main/java/kotlingrundlagen/objectorientierten/FunktionenKonstruktor.kt
@@ -0,0 +1,31 @@
+package kotlingrundlagen.objectorientierten
+    class Employee {
+        // Üye değişkenler
+        var name: String = ""
+        var age: Int = 0
+        var position: String = ""
+
+        // Birincil constructor
+        constructor(name: String, age: Int, position: String) {
+            this.name = name
+            this.age = age
+            this.position = position
+        }
+
+        // Üye fonksiyon (method)
+        fun introduceYourself() {
+            println("Merhaba, benim adım $name, $age yaşındayım ve $position pozisyonundayım.")
+        }
+    }
+
+    fun main() {
+        // Employee sınıfından iki çalışan örneği oluştur
+        val employee1 = Employee("Ahmet", 30, "Yazılım Mühendisi")
+        val employee2 = Employee("Mehmet", 25, "İş Analisti")
+        val employee3 = Employee("Sedat", 32, "Bilgisayar Mühendisi")
+
+        // Çalışanları tanıt
+        employee1.introduceYourself()
+        employee2.introduceYourself()
+        employee3.introduceYourself()
+    }
